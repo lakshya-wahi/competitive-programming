74129d8f5ea71a737f0dcc5e53ef1b1e4f5a8e60
Lakshya Wahi <142634369+lakshya-wahi@users.noreply.github.com>
2026-07-21 15:35:02 -0700

    Update 1985E-Secret-Box.cpp

--- codeforces/counting-and-math/1985E-Secret-Box.cpp
+++ codeforces/counting-and-math/1985E-Secret-Box.cpp
@@ -1,1 +1,33 @@
+// RATING: 1200
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        
+        long long k;
+        cin >> k;
+        
+        long long maxNum = 0;
+        for (int a = 1; a <= x; a++) {
+            for (int b = 1; b <= y; b++) {
+                if (k % (a * b) == 0) {
+                    long long c = k / (a * b);
+                    if ((x - a + 1) * (y - b + 1) * (z - c + 1) > maxNum) {
+                        maxNum = (x - a + 1) * (y - b + 1) * (z - c + 1);
+                    }
+                }
+            }
+        }
+        
+        cout << maxNum << endl;
+    }
+}
